68be694dd290bda53f05020b24f8c1a6a05a25ea
liujia <marcous@126.com>
2016-04-16 10:45:02 +0800

    add a class a method

--- app/src/main/java/com/example/marcous/githubtest/MainActivity.java
+++ app/src/main/java/com/example/marcous/githubtest/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.marcous.githubtest.utils.TextUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        updateText();
+    }
+
+    private void updateText() {
+        TextUtils textUtils=new TextUtils();
+        ((TextView)findViewById(R.id.tv_msg)).setText(textUtils.getValue());
+        for (int i=0;i<10;i++){
+            Log.d("MainActivity","i="+i);
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/marcous/githubtest/utils/TextUtils.java
@@ -0,0 +1,10 @@
+package com.example.marcous.githubtest.utils;
+
+/**
+ * Created by liujia on 16/4/16.
+ */
+public class TextUtils {
+    public   String getValue(){
+        return "I am a Boy!";
+    }
+}
